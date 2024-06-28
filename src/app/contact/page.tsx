ddedb1aa279bf46d3d68a01113d10d237436c147
shubhamcypher <shubhamdevo23@gmail.com>
2024-06-28 23:54:50 +0530

    committed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,7 +12,7 @@ function page() {
         ></div>
         <div className="text-white relative px-4 py-10 bg-black shadow-lg sm:rounded-3xl sm:p-20">
           <div className="text-center pb-6">
-            <h1 className="text-3xl text-teal-700 font-bold">Contact U</h1>
+            <h1 className="text-3xl text-teal-700 font-bold">Contact Us</h1>
             <p className="text-gray-300">
               Fill up the form below to send us  message.
             </p>
@@ -20,25 +20,25 @@ function page() {
 
           <form>
             <input
-              className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-800"
               type="text"
               placeholder="Name"
               name="name"
             />
             <input
-              className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-800"
               type="email"
               placeholder="Email"
               name="email"
             />
             <input
-              className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-800"
               type="number"
               placeholder="Phone No"
               name="Phone_No"
             />
             <textarea
-              className="shadow mb-4 min-h-0 appearance-none border rounded h-64 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow mb-4 min-h-0 appearance-none border rounded h-64 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-gray-800"
               placeholder="Type your message here..."
               name="message"
               style={{ height: '121px' }}
